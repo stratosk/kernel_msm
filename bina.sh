1b3bc260287f33b8bbbb176ef9a879794a02be3b
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-01-30 07:46:43 +0800

    v2.0.5 build update

--- bina.sh
+++ bina.sh
@@ -9,7 +9,7 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="kampung-pandan"
-export LOCALVERSION="-Semaphore-Pandan-CM11-N4_2.0.2"
+export LOCALVERSION="-Semaphore-Pandan-CM11-N4_2.0.5"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
